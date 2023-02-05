21a1b156b5914d344964a7c9e1891339d4ad46a2
cvennemann <christopher.vennemann@web.de>
2023-02-05 11:20:50 +0100

    fixed some type and naming errors, implemented roll generator

--- source-code/rollgenerator.py
+++ source-code/rollgenerator.py
@@ -7,10 +7,41 @@ class Rollgenerator(abc.ABC):
         pass
 
 
-class StringToRollGenerator:
-    def __init__(self, list_of_rolls: [int]):
-        self.__list_of_rolls = []
+class StringToRollGenerator(Rollgenerator):
+    def __init__(self, string_of_rolls: str):
+        self.__string_of_rolls = self.__validate_string(string_of_rolls)
+        self.__rolls_as_int = self.__string_to_int()
+
+    @staticmethod
+    def __validate_string(string_of_rolls):
+        if type(string_of_rolls) != str:
+            raise TypeError()
+
+        for char in string_of_rolls:
+            if char not in '123456789/-X ':
+                raise ValueError(f"Unexpected character: {char}")
+
+        return string_of_rolls
+
+    def __string_to_int(self):
+        rolls_as_int = []
+        for char in self.__string_of_rolls:
+            int_char = None
+            if char == ' ':
+                continue
+            elif char.isdecimal():
+                int_char = int(char)
+            elif char == 'X':
+                int_char = 10
+            elif char == '-':
+                int_char = 0
+            elif char == '/':
+                int_char = 10 - rolls_as_int[-1]
+
+            rolls_as_int.append(int_char)
+
+        return rolls_as_int
 
     def get_next_roll(self):
-        for roll in [-1, -1, -1, -1, -1]:
-            yield roll
+        for number in self.__rolls_as_int:
+            yield number
--- test/test_rollgenerator.py
+++ test/test_rollgenerator.py
@@ -2,14 +2,12 @@ from unittest import TestCase
 from rollgenerator import StringToRollGenerator
 
 
-class TestRollGenerator(TestCase):
-    def test_yield_next_roll(self):
+class TestStringToRollGenerator(TestCase):
+    def test__get_next_roll(self):
         test_string = '5/ -5 4/ -- 6/ 25 7/ X 12 34'
         goal_list = [5, 5, 0, 5, 4, 6, 0, 0, 6, 4, 2, 5, 7, 3, 10, 1, 2, 3, 4]
 
         roll_gen = StringToRollGenerator(test_string)
-        test_list = [roll for roll in roll_gen.yield_next_roll()]
+        test_list = [roll for roll in roll_gen.get_next_roll()]
 
         self.assertEquals(test_list, goal_list)
-
-
